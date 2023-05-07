6e1588b6f847e925a5529e652f146e7d8e39baaa
Muske2 <98347170+Muske2@users.noreply.github.com>
2023-05-07 22:43:27 +0530

    Update knapsack_backtrack.cpp

--- LAB9/knapsack_backtrack.cpp
+++ LAB9/knapsack_backtrack.cpp
@@ -1,40 +1,59 @@
+#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-
-int n, W, maxValue = INT_MIN;
-vector<int> w, v, ans;
-
-void knapsack(int idx, int curW, int curV, vector<int> &curAns) {
-    if (curW > W) return;
-    if (idx == n) {
-        if (curV > maxValue) {
-            maxValue = curV;
-            ans = curAns;
+vector<int>ans,weight,profit;
+int capacity,n,maxval=-1;
+void knapsack(int index,vector<int>&curans,int curW,int curP)
+{
+    if(curW>capacity)
+    {
+        return;
+    }
+    if(n==index)
+    {
+        if(curP>maxval)
+        {
+            maxval=curP;
+            ans=curans;
         }
         return;
     }
-    curAns[idx] = 0;
-    knapsack(idx+1, curW, curV, curAns);
-    curAns[idx] = 1;
-    knapsack(idx+1, curW+w[idx], curV+v[idx], curAns);
+    curans[index]=0;
+    knapsack(index+1,curans,curW,curP);
+    curans[index]=1;
+    knapsack(index+1,curans,curW+weight[index],curP+profit[index]);
 }
-
-int main() {
-    cin >> n >> W;
-    w.resize(n);
-    v.resize(n);
+int main()
+{
+    cout<<"Enter number of items: "<<endl;
+    cin>>n;
+    profit.resize(n);
+    weight.resize(n);
     ans.resize(n);
-    for (int i = 0; i < n; i++) {
-        cin >> w[i] >> v[i];
+    cout<<"Enter capacity of knapsack: "<<endl;
+    cin>>capacity;
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter profit of item "<<i+1<<" : "<<endl;
+        cin>>profit[i];
+        cout<<"Enter weight of item "<<i+1<<" : "<<endl;
+        cin>>weight[i];
     }
-    vector<int> curAns(n);
-    knapsack(0, 0, 0, curAns);
-    cout << "Max Value: " << maxValue << endl;
-    cout << "Selected items: ";
-    for (int i = 0; i < n; i++) {
-        if (ans[i] == 0) continue; // skip items that were not selected
-        cout << i+1 << " "; // output 1-indexed items
+    vector<int>curans(n);
+    knapsack(0,curans,0,0);
+    cout<<"Total profit: "<<maxval<<endl;
+    cout<<"Items selected: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<"x"<<i+1<<"   ";
     }
-    cout << endl;
-    return 0;
+    cout<<endl;
+    for(int i=0;i<n;i++)
+    {
+        if(ans[i]==0)
+            cout<<"0    ";
+        else
+            cout<<"1    ";
+    }
+    cout<<endl;
 }
