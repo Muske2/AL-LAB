9592fae8740a78c83727b906199f2693ad6048b2
Muske2 <98347170+Muske2@users.noreply.github.com>
2023-05-08 15:32:13 +0530

    Update knapsack_branch_n_bound.cpp

--- LAB10/knapsack_branch_n_bound.cpp
+++ LAB10/knapsack_branch_n_bound.cpp
@@ -1,108 +1,84 @@
 #include<iostream>
-#include<conio.h>
+#include<bits/stdc++.h>
 using namespace std;
-class knapsack
+int n,finalprofit=0,finalweight=0,capacity;
+vector<int>profit,weight,selected,sel;
+int Bound(int,int,int);
+int Bound(int k,int cp,int cw)
 {
-	private:
-		int p[20],w[20],m,x[20],y[20],n;
-		int Bound(int,int,int);
-		int fp,fw;
-
-	public:
-		knapsack()
-		{
-			fp=0;
-		}
-		void BKnap(int,int,int);
-		void setdata();
-		void getdata();
-};
-void knapsack::setdata()
-{
-	int i;
-
-	cout<<"\nEnter number of elements: ";
-	cin>>n;
-
-	cout<<"\nEnter elements in the descending order of P/W\n";
-
-	for(i=1;i<=n;i++)
-	{
-		cout<<"\nEnter profit of "<<i<<" element: ";
-		cin>>p[i];
-		cout<<"\nEnter weight of "<<i<<" element: ";
-		cin>>w[i];
-	}
-
-	cout<<"\nEnter capacity of knapsack: ";
-	cin>>m;
+    int a,b;
+    a=cp;
+    b=cw;
+    for(int i=k+1;i<=n;i++)
+    {
+        b=b+weight[i];
+        if(b<=capacity)
+        {
+            a=a+profit[i];
+        }
+        else
+            return (a+(1-(b-capacity)/weight[i])*profit[i]);
+    }
+    return a;
 }
-
-int knapsack::Bound(int cp,int cw,int k)
+void Knap(int k,int cp,int cw)
 {
-	int b,c,i;
-	b=cp;
-	c=cw;
-
-	for(i=k+1;i<=n;i++)
-	{
-		c=c+w[i];
-		if(c<m)
-			b=b+p[i];
-		else
-			return (b+(1-(c-m)/w[i]) * p[i]);
-	}
-	return b;
+    if(cw+weight[k]<=capacity)
+    {
+        sel[k]=1;
+        if(k<n)
+        {
+            Knap(k+1,cp+profit[k],cw+weight[k]);
+        }
+        if(cp+profit[k]>finalprofit && k==n)
+        {
+            finalprofit=cp+profit[k];
+            finalweight=cw+weight[k];
+            for(int j=1;j<=k;j++)
+            {
+                selected[j]=sel[j];
+            }
+        }
+    }
+    if(Bound(k,cp,cw)>=finalprofit)
+    {
+        sel[k]=0;
+        if(k<n)
+        {
+            Knap(k+1,cp,cw);
+        }
+        if(cp>finalprofit && k==n)
+        {
+            finalprofit=cp;
+            finalweight=cw;
+            for(int j=1;j<=k;j++)
+            {
+                selected[j]=sel[j];
+            }
+        }
+    }
 }
-
-void knapsack::BKnap(int k,int cp,int cw)
-{
-	int j;
-	if(cw+w[k]<=m)
-	{
-		y[k]=1;
-		if(k<n)
-			BKnap(k+1,cp+p[k],cw+w[k]);
-		if((cp+p[k]>fp) && (k==n))
-		{
-			fp=cp+p[k];
-			fw=cw+w[k];
-			for(j=1;j<=k;j++)
-				x[j]=y[j];
-		}
-	}
-
-	if(Bound(cp,cw,k)>=fp)
-	{
-		y[k]=0;
-		if(k<n)
-			BKnap(k+1,cp,cw);
-		if((cp>fp) && (k==n))
-		{
-			fp=cp;
-			fw=cw;
-			for(j=1;j<=k;j++)
-				x[j]=y[j];
-		}
-	}
-}
-
-void knapsack::getdata()
-{
-	int i;
-
-	for(i=1;i<=n;i++)
-		cout<<"x["<<i<<"]= "<<x[i]<<endl;
-}
-
 int main()
 {
-	knapsack k;
-
-	k.setdata();
-
-	k.BKnap(1,0,0);
-
+    cout<<"\nEnter number of elements: ";
+	cin>>n;
+	profit.resize(n);
+	weight.resize(n);
+	selected.resize(n);
+	sel.resize(n);
+	cout<<"\nEnter elements in the descending order of P/W\n";
+	for(int i=1;i<=n;i++)
+	{
+		cout<<"\nEnter profit of "<<i<<" element: ";
+		cin>>profit[i];
+		cout<<"\nEnter weight of "<<i<<" element: ";
+		cin>>weight[i];
+	}
+	cout<<"\nEnter capacity of knapsack: ";
+	cin>>capacity;
+	Knap(1,0,0);
+	cout<<"PROFIT: "<<finalprofit<<endl;
 	cout<<"\nSolution is :\n";
-	k.getdata();
+	for(int i=1;i<=n;i++)
+		cout<<"x["<<i<<"]= "<<selected[i]<<endl;
 }
